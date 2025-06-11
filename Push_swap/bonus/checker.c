492aa2999e2682d42d0ecdc5a15e45926e5e4e55
Mateus Ferreira Coelho Provetti Leite <mateferr@c1r6s5.42lisboa.com>
2025-06-11 12:35:22 +0100

    0611

--- Push_swap/bonus/checker.c
+++ Push_swap/bonus/checker.c
@@ -6,7 +6,7 @@
 /*   By: mateferr <mateferr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 11:37:52 by mateferr          #+#    #+#             */
-/*   Updated: 2025/06/04 15:59:36 by mateferr         ###   ########.fr       */
+/*   Updated: 2025/06/11 12:34:44 by mateferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ int	main(int c, char **v)
 	stk_a = NULL;
 	stk_b = NULL;
 	if (!input_validation(&stk_a, c, v))
-		return (free_list(&stk_a), free_list(&stk_b), write(2, "Error\n", 6), 1);
+		return (free_list(&stk_a), free_list(&stk_b), write(2, "Error\n", 6),
+			1);
 	if (!read_instructions(&stk_a, &stk_b))
 		return (write(2, "Error\n", 6), 1);
 	if (is_sorted(stk_a, stk_b))
